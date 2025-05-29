2cbc9744525a353ea5254212dabaab7bf116dd00
Denny898 <makushadennis889@gmail.com>
2025-05-29 10:49:51 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@
 const chalk = require("chalk")
 const fs = require("fs")
 
-global.ownerNumber = ["263786115435@s.whatsapp.net"]
+global.ownerNumber = ["263710781795@s.whatsapp.net"]
 global.botnumber = "263786115435"
 global.autotyping = false;
 global.autorecording = false;
 global.botnumber = ['263786115435']
-global.DevName = "iconic tech"
-global.ownerName = "iconic tech"
+global.DevName = "DBY"
+global.ownerName = "DBY"
 global.Botname = "Terminator QR MD"
 global.versionBot = "𝟏"
 global.packname = "iconic tech"
